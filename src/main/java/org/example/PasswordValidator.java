df5e1cfee4fd946c15e2129197f735c0cfb3f118
KroegerLeif <Leif.kr@outlook.com>
2025-08-27 11:46:04 +0200

    Checks for the Min length of the Passwort

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -1,4 +1,26 @@
 package org.example;
 
-public class PasswordValidator {
+public final class PasswordValidator {
+
+    //Checks MinLength of The Password
+    public static boolean hasMinLength(String password, int minLength){
+        if(password == null || password.isEmpty()){
+            //TODO Gives a hint to the user that the password enty was wrong
+            return false;
+        }else {
+            return  password.length() >= minLength;
+        }
+    }
+
+    public static boolean containsDigit(String password){return false;};
+    public static boolean containsUpperCaseAndLowerCase(String password){return false;};
+    public static boolean isCommonPassword(String password){return false;};
+
+    //TODO Bonus Aufgaben
+    public static boolean containsSpecialCharacter(String password){return false;};
+
+
+    //Ruft die Anderen Methoden auf
+    public static boolean isValid(String password){return false;};
+
 }
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,2 +1,98 @@
+import org.example.PasswordValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordValidatorTest {
+
+    //3.1 Mindestlänge
+    @Test
+    void testMinLength_checksCorrectLength_givenCorrectLength() {
+        //Given
+        String[] passwords = {
+                "1234567",
+                "12345678",
+                "123456789"
+        };
+
+        int[] minLength = {
+                7,
+                8,
+                9
+        };
+
+        boolean[] expected = {
+                true,
+                true,
+                true
+        };
+
+        //When
+        boolean[] actual = new boolean[passwords.length];
+        for(int i = 0; i < actual.length; i++) {
+            actual[i] = PasswordValidator.hasMinLength(passwords[i], minLength[i]);
+        }
+
+        //Then
+        for(int i = 0; i < actual.length; i++) {
+            assertEquals(expected[i], actual[i]);
+        }
+    }
+
+    @Test
+    void testMinLength_checksCorrectLength_givenIncorrectLength() {
+        //Given
+        String[] passwords = {
+                "1234567",
+                "12345678",
+                "123456789"
+        };
+
+        int[] minLength = {
+                9,
+                10,
+                11
+        };
+
+        boolean[] expected = {
+                false,
+                false,
+                false
+        };
+
+        //When
+        boolean[] actual = new boolean[passwords.length];
+        for(int i = 0; i < actual.length; i++) {
+            actual[i] = PasswordValidator.hasMinLength(passwords[i], minLength[i]);
+        }
+
+        //Then
+        for(int i = 0; i < actual.length; i++) {
+            assertEquals(expected[i], actual[i]);
+        }
+    }
+
+    @Test
+    void tesMinLength_checksForNull_givenANullString(){
+        //Given
+        String password = null;
+        int minLength = 10;
+        boolean expected = false;
+        //When
+        boolean actual = PasswordValidator.hasMinLength(password, minLength);
+        //Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMinLength_cheksEmptyString_givenEmptyString(){
+        //Given
+        String password = "";
+        int minLength = 0;
+        boolean expected = false;
+        //When
+        boolean actual = PasswordValidator.hasMinLength(password, minLength);
+        //Then
+        assertEquals(expected, actual);
+    }
 }
